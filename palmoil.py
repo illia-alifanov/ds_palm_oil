7e6b1e9608d1da33c974bf2aea39f8dcb35d9485
Illia Alifanov <aliphanov.i@gmail.com>
2020-02-23 21:18:33 +0200

    plot regions production

--- palmoil.py
+++ palmoil.py
@@ -6,14 +6,15 @@ import matplotlib.pyplot as plt
 #plt.style.use('fivethirtyeight')
 import pandas as pd
 #import statsmodels.api as sm
-import matplotlib
+#import matplotlib
+import array as arr
 
 
 def getExcelSheet(fileName, sheetName):
     production_ = pd.read_excel(fileName, sheetName)
 
-    production_['date'] = production_['Year'].apply(str) + '-' + production_['Month'].apply(str) + '-01'
-    production_['date'] = production_['date'].apply(pd.to_datetime)
+    production_['Date'] = production_['Year'].apply(str) + '-' + production_['Month'].apply(str) + '-01'
+    production_['Date'] = production_['Date'].apply(pd.to_datetime)
     return production_
 
 def main():
@@ -21,8 +22,20 @@ def main():
     sheetName = 'MonthlyProduction'
     production = getExcelSheet(fileName, sheetName)
 
+    regions = ['JHR', 'PHG', 'PRK', 'SBH', 'SWK', 'OTHERPEN']
 
-    print(production)
+    for r in regions:
+        plt.plot(production['Date'], production[r], label=r) #, marker='o'
+ 
+    
+    plt.xlabel('Production date')
+    plt.ylabel('Production vs Date')
+    plt.grid(True)
+    plt.legend(loc='lower right')
+
+    plt.show()
+
+    #print(production)
 
 main()
 
